6dd6e7ca775604c0f6d42670594570387d9bd6f0
jgysland <jake.gysland@gmail.com>
2016-03-25 13:56:36 -0400

    merged parts of app.py from devel

--- app.py
+++ app.py
@@ -16,4 +16,22 @@ def show_resume():
 
 
 if __name__ == '__main__':
-    app.run()
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--debug', default=False, action='store_true',
+                        help='Start flask app in debug mode. Default false.')
+    parser.add_argument('-p', '--port', default=5000, type=int,
+                        help="Port to run app on. Default 5000.")
+    parser.add_argument('-H', '--host', default='127.0.0.1',
+                        help='Set the flask app\'s "host" parameter. Default '
+                        '127.0.0.1 answers requests only on localhost. '
+                        '0.0.0.0 answers requests from any host. Use with '
+                        'caution, especially in conjunction with --debug!')
+    parser.add_argument('-r', '--resume-path', default='data/resume.json',
+                        help='Set the (relative) path of JSON file containing '
+                        'resume data. Default "data/resume.json".')
+    args = parser.parse_args()
+
+    resume_path = args.resume_path
+
+    app.run(host=args.host, port=args.port, debug=args.debug)
